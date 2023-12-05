897c5398fd5c7ed39577c4c5019f0818576dc5ae
Muh. Fauzi Ramadhan N <fauzinugraha06@gmail.com>
2023-12-05 15:15:12 +0700

    Add Auth Screen

---
+++ lib/features/auth/view/auth_screen.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+import 'package:mobileapp_scabies/core/constants/colors.dart';
+import 'package:mobileapp_scabies/core/constants/font_size.dart';
+import 'package:mobileapp_scabies/core/constants/font_weigth.dart';
+
+class AuthScreen extends StatelessWidget {
+  const AuthScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            width: double.infinity,
+            height: MediaQuery.of(context).size.height * 0.5,
+            alignment: Alignment.center,
+            decoration: const BoxDecoration(
+              color: AppColors.brandColor,
+              borderRadius: BorderRadius.vertical(
+                bottom: Radius.elliptical(500, 200),
+              ),
+              image: DecorationImage(
+                image: AssetImage(
+                  "assets/images/auth_image.png",
+                ),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 60.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    'Laras App',
+                    style: TextStyle(
+                      fontSize: 32,
+                      fontWeight: FontWeight.w900,
+                      color: AppColors.brandColor,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 22.0,
+                  ),
+                  const Text(
+                    'Aplikasi Pendeteksi Dini Penyakit Kulit Scabies',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: AppColors.brandColor,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 82.0,
+                  ),
+                  //set consumer buat login
+                  Container(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 16.0, vertical: 12.0),
+                    decoration: BoxDecoration(
+                      color: AppColors.brandColor,
+                      borderRadius: BorderRadius.circular(6.0),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Image.asset(
+                          "assets/images/google.png",
+                          width: 24,
+                          height: 24,
+                        ),
+                        const SizedBox(
+                          width: 8.0,
+                        ),
+                        const Text(
+                          "Masuk Dengan Gmail",
+                          style: TextStyle(
+                            fontSize: AppFontSize.actionMedium,
+                            fontWeight: AppFontWeight.actionSemiBold,
+                            color: AppColors.white,
+                            letterSpacing: 0.75,
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 20.0,
+          ),
+        ],
+      ),
+    );
+  }
+}
